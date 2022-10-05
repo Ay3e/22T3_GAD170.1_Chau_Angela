c3da7d75d2c7e248b51ff67a35b53dbc1e06285f
Ay3e <angela.chau@students.mq.edu.au>
2022-10-06 07:27:31 +1100

    Adding Comments

--- Platformer_Framework-main/Assets/Scripts by (Angela Chau)/LevellingSystem.cs
+++ Platformer_Framework-main/Assets/Scripts by (Angela Chau)/LevellingSystem.cs
@@ -25,23 +25,31 @@ namespace WriteYourNameHere
             // TODO XP 6/13: Set our current XP Threshold to be our level multiplied by our 100.
             experienceThreshold = level * 100;
             // TODO XP 7/13: Debug out our starting values of our level, XP and current XP threshold.
-            Debug.Log("Level: " + level + "Exp: " + experience + "XP required to level up: " + experienceThreshold);
+            Debug.Log("Level: " + level + " Exp: " + experience + " Exp required to level up: " + experienceThreshold);
             // TODO XP 8/13: Increase the current XP by a random amount between 50 and 100.
-            experience = 0 + 100;
+            experience = experience + 100;
             // TODO XP 9/13: Debug out our current XP.
             Debug.Log("Exp: " + experience);
             // TODO XP 10/13: Check if our current XP is more than our threshold.
-            // TODO XP 11/13: If it is, then let's increase out level by one.
-            // TODO XP 12/13: Let's also increase recalculate our current XP threshold as we've levelled up.
             if (experience > experienceThreshold)
             {
+                // TODO XP 11/13: If it is, then let's increase out level by one.
                 level += 1;
+                // TODO XP 12/13: Let's also increase recalculate our current XP threshold as we've levelled up.
                 experienceThreshold = level * 100;
             }
             // TODO XP 13/13: Debug out our new level value, as well as our current XP and the next threshold we need to hit.
-            Debug.Log("Level: " + level + "Exp: " + experience + "XP required to level up: " + experienceThreshold);
+            Debug.Log("Level: " + level + " Exp: " + experience + " Exp required to level up: " + experienceThreshold);
             // TODO XP Final: Add code comments describing what you hope your code is doing throughout this script.
-
+            /*
+             * Line: 22 & 24    |   Player starts at level 1 with no experience
+             * Line: 26         |   The Exp threshold is calulated 
+             * Line: 28         |   "Level: 1 Exp: 0 Exp required to level up: 100" will be displayed   
+             * Line: 30         |   The player gains 100 experience
+             * Line: 32         |   "Exp: 100" will be displayed
+             * Line: 34 - 39    |   If the Player gets more than 100 experience they will level up and a new threshold is calculated
+             * Line: 42         |   "Level: 2 Exp: 100 Exp required to level up: 200" will be displayed
+             */
             // TODO XP Bonus: Adjust our character's stats ("runSpeed" and/or "jumpStrength") based on their level. (Hint: You'll need a reference to the SimpleCharacterController script!)
 
         }
--- Platformer_Framework-main/Assets/Scripts by (Angela Chau)/SimpleCharacterController.cs
+++ Platformer_Framework-main/Assets/Scripts by (Angela Chau)/SimpleCharacterController.cs
@@ -9,7 +9,6 @@ namespace AngelaChau
     /// </summary>
     public class SimpleCharacterController : MonoBehaviour
     {
-        [SerializeField] private float verticalInputValue; // The value of our vetrical input axis.
         [SerializeField] private float horizontalInputValue; // The value of our horizontal input axis.
         [SerializeField] private SpriteRenderer spriteRenderer; // Our character's sprite.
 
@@ -19,9 +18,10 @@ namespace AngelaChau
         private float runSpeed = 3f;
         // TODO Movement 3/8: Declare a variable for the strength of our jump.
         private float jumpStrength = 5f;
+
         private void Awake()
         {
-            spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+            //Reference of rigidbody to make changes
             rbody2D = transform.GetComponent<Rigidbody2D>();
         }
 
@@ -31,19 +31,26 @@ namespace AngelaChau
             horizontalInputValue = 0f;
             // TODO Movement 5/8: Transform our character's position on the X axis. (Reference our stored horizontal input value here!) 
             horizontalInputValue = Input.GetAxisRaw("Horizontal");
-            //horizontalInputValue = Input.GetAxisRaw("Horizontal"); COLLECTS PLAYER INPUT FROM THEIR KEYBOARD PRESS 'A' AND 'D' TO MOVE HORIZONTALLY
             transform.position += new Vector3(horizontalInputValue * runSpeed, 0, 0) * Time.deltaTime;
-            //transform.position += new Vector3(horizontalInputValue * runSpeed, 0, 0) * Time.deltaTime; CHANGES THE CHARACTER POSITION BASED ON REAL TIME
             // TODO Movement 6/8: Check if the player presses the "Jump" button (by default, the space bar on the keyboard).
-            // TODO Movement 7/8: If they do, then add vertical velocity to our rigidbody to make our character "jump"!
             if (Input.GetKeyDown(KeyCode.Space))
             {
+                // TODO Movement 7/8: If they do, then add vertical velocity to our rigidbody to make our character "jump"!
                 rbody2D.velocity = Vector2.up * jumpStrength;
             }
             // TODO Movement 8/8: Add this script to a game object and make a new prefab from it, and explore the level!
-
+            //Script on Chas Level 1
             // TODO Movement Final: Add code comments describing what you hope your code is doing throughout this script.
-
+            /*
+             * Line: 16         |
+             * Line: 18         |
+             * Line: 20         |
+             * Line: 22 - 25    |
+             * Line: 31         |
+             * Line: 33         |
+             * Line: 34         |
+             * Line: 36 - 39    |
+             */
             // TODO Movement Bonus 1: Ensure that our character can only jump if they are "grounded". (Hint: You can use a boolean as a part of this!)
 
             // TODO Movement Bonus 2: Flip our character's sprite so that it faces left/right if we are moving left/right. (Hint: A SpriteRenderer reference, and changing its FlipX = true/false will help!)
