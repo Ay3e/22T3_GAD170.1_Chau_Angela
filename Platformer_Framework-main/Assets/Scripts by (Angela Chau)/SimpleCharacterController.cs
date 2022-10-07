c194aac0f2cda3d393048f3ef50cd8a83ef786f8
Ay3e <angela.chau@students.mq.edu.au>
2022-10-07 14:53:36 +1100

    Finalising Scripts
    * SimpleCharacterControl

--- Platformer_Framework-main/Assets/Scripts by (Angela Chau)/SimpleCharacterController.cs
+++ Platformer_Framework-main/Assets/Scripts by (Angela Chau)/SimpleCharacterController.cs
@@ -15,14 +15,13 @@ namespace AngelaChau
         // TODO Movement 1/8: Declare a variable for a reference to our 2D rigidbody, for physics stuff.
         private Rigidbody2D rbody2D;
         // TODO Movement 2/8: Declare a variable for the speed we can run at in Unity-units-per-second.
-        private float runSpeed = 3f;
+        private float runSpeed = 20f;
         // TODO Movement 3/8: Declare a variable for the strength of our jump.
-        private float jumpStrength = 5f;
+        private float jumpStrength = 3f;
 
         private void Awake()
         {
-            //Reference of rigidbody to make changes
-            rbody2D = transform.GetComponent<Rigidbody2D>();
+            rbody2D = transform.GetComponent<Rigidbody2D>(); //(Code Monkey, 2019, 3:53 - 4:10)
         }
 
         private void Update()
@@ -42,14 +41,14 @@ namespace AngelaChau
             //Script on Chas Level 1
             // TODO Movement Final: Add code comments describing what you hope your code is doing throughout this script.
             /*
-             * Line: 16         |
-             * Line: 18         |
-             * Line: 20         |
-             * Line: 22 - 25    |
-             * Line: 31         |
-             * Line: 33         |
-             * Line: 34         |
-             * Line: 36 - 39    |
+             * Line: 16         |allows you to make changes in the rigidbody so you can go against gravity for the player to jump
+             * Line: 18         |sets the speed of the character
+             * Line: 20         |sets how high the character can jump
+             * Line: 22 - 25    |I honestly don't know what it does but it makes jumping work, without it the character can't jump
+             * Line: 31         |The horizontal input value, as mentioned we can access it later on.
+             * Line: 33         |Allows player input to move horizontally using 'A' or 'D' 
+             * Line: 34         |Is the code that makes the gameobject move accordingly to the keys pressed
+             * Line: 36 - 39    |the script to make the player jump, however it needs the awake function to work line 22-25
              */
             // TODO Movement Bonus 1: Ensure that our character can only jump if they are "grounded". (Hint: You can use a boolean as a part of this!)
 
